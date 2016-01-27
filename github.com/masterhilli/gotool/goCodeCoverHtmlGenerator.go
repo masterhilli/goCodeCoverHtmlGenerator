2fff7c6c7d2ca1ba2ce371ebc98dfbf65a81267c
masterhilli <martin.hillbrand@gmail.com>
2016-01-27 08:56:53 +0100

    added os independet copy, from Stackoverflow.

--- github.com/masterhilli/gotool/goCodeCoverHtmlGenerator.go
+++ github.com/masterhilli/gotool/goCodeCoverHtmlGenerator.go
@@ -19,6 +19,7 @@ package main
 
 import (
     "fmt"
+    "io"
     "os"
     "os/exec"
     "path/filepath"
@@ -51,6 +52,10 @@ var paramsForTestWithCodeCoverage []string = []string{boolTestParam, boolCodeCov
 const POSSIBLE_ARG_COUNT int = 2
 const POS_MAIN_PACKAGE_NAME int = 1
 var packageNameForGoTool string = `github.com`
+
+// regexp constants
+const REGEXP_GO_FILES string = ".*\\.go"
+const REGEXP_GO_TEST_FILES string = ".*test\\.go"
 
 func main() {
     retrieveMainPackageName()
@@ -123,25 +128,33 @@ func getDirectoriesOfPath(path string) []string{
 }
 
 func directoryHasGoTestFiles(path string) bool {
+    paths := getGoFilePathsFromDirectory(path, REGEXP_GO_TEST_FILES)
+    return len(paths) > 0
+}
+
+func getGoFilePathsFromDirectory(path string, regexForFile string) []string{
     fileInfo, err := ioutil.ReadDir(path)
     if err != nil {
         panic(err)
     }
-
+    var names []string = make([]string, 0)
+    path = addSeparator(path)
     for i := range fileInfo {
         if !fileInfo[i].IsDir() {
-            matchGoTest, _ := regexp.MatchString(".*test\\.go", fileInfo[i].Name())
-            if matchGoTest {
-                return true
+            matchesGoFile, _ := regexp.MatchString(regexForFile, fileInfo[i].Name())
+
+            if matchesGoFile {
+                names = append(names, fileInfo[i].Name())
             }
         }
     }
-    return false
+    return names
 }
 
 func createCoverageFile() string{
     executeTestWithCoverageInCurrentFolder()
     codeCoverageFile := openCodeCoverageOutputFile()
+    defer codeCoverageFile.Close()
     relativeCodeCoverFileContent := makePathsRelativeForContentIn(codeCoverageFile)
     codeCoverageFile.Close()
     writeContentToCodeCoverageFile("mode: set\n" + relativeCodeCoverFileContent)
@@ -150,11 +163,11 @@ func createCoverageFile() string{
 
 func copyNewGoFilesToGoRootSrcWhenInSeparateLocation() {
     // TODO create method get the package go import path
-    var pathToCopyToGoRootFolder string
+    var relativePathToCurrentPackage string
     currentPath,_ := filepath.Abs(".")
     indexForPackageStart := strings.Index(currentPath, packageNameForGoTool)
     if (indexForPackageStart > 0) {
-        pathToCopyToGoRootFolder = currentPath[indexForPackageStart:len(currentPath)]
+        relativePathToCurrentPackage = currentPath[indexForPackageStart:len(currentPath)]
     } else {
         fmt.Printf("ERROR: Could not retrieve index for provided package in current path.\n\tpath:\t%s\n\tpkg:\t%s\n",
                    currentPath, packageNameForGoTool)
@@ -162,14 +175,15 @@ func copyNewGoFilesToGoRootSrcWhenInSeparateLocation() {
     }
 
     goPathSrc := createPathToGoPathSrc()
-    if(isCurrentExecutionPathAlreadyInGoRootSrc(currentPath, goPathSrc)) {
+    if(!isCurrentExecutionPathAlreadyInGoRootSrc(currentPath, goPathSrc)) {
         // TODO own method to create the path to package under libraries
-        packageFolderInGoPath := goPathSrc + pathToCopyToGoRootFolder
-        err := os.MkdirAll(packageFolderInGoPath, 0777)
+        packageFolderInEnvVariableGoPath := goPathSrc + relativePathToCurrentPackage
+        fmt.Printf("current: %s\ngoPathSrc: %s\n", currentPath, packageFolderInEnvVariableGoPath)
+        err := os.MkdirAll(packageFolderInEnvVariableGoPath, 0777)
         if err != nil { panic(err)}
 
 
-        copyFilesToGOROOTPath(packageFolderInGoPath)
+        copyFilesToGOROOTPath(packageFolderInEnvVariableGoPath)
     }
 }
 
@@ -238,15 +252,73 @@ func writeContentToCodeCoverageFile(relativeCodeCoverFileContent string) {
     }
 }
 
-func copyFilesToGOROOTPath(packageFolderInGoPath string) {
-    commandToAllFilesFromCurrentFolder := exec.Command("xcopy.exe", "/Y", "*.go", packageFolderInGoPath)
-    output, err := commandToAllFilesFromCurrentFolder.Output()
-    if (err != nil) {
-        panic(err)
+func copyFilesToGOROOTPath(packageFolderInEnvVariableGoPath string) {
+    filenames := getGoFilePathsFromDirectory(".", REGEXP_GO_FILES)
+    for i := range filenames {
+        cpFrom := addSeparator(".") + filenames[i]
+        cpTo := addSeparator(packageFolderInEnvVariableGoPath) + filenames[i]
+        err := copyFile(cpFrom, cpTo)
+        if (err != nil) {
+            panic(err)
+        }
+
     }
-    fmt.Println(string(output))
 }
 
 func isCurrentExecutionPathAlreadyInGoRootSrc(currentPath, goPathSrc string) bool {
     return strings.Index(currentPath, goPathSrc) == 0
+}
+
+
+// Copy file content:
+// CopyFile copies a file from src to dst. It always overrides the content
+// copied and changed that piece of code from: http://stackoverflow.com/questions/21060945/simple-way-to-copy-a-file-in-golang
+// by markc
+func copyFile(src, dst string) (err error) {
+    sfi, err := os.Stat(src)
+    if err != nil {
+        panic (err)
+        return
+    }
+    if !sfi.Mode().IsRegular() {
+        // cannot copy non-regular files (e.g., directories,
+        // symlinks, devices, etc.)
+        return fmt.Errorf("CopyFile: non-regular source file %s (%q)", sfi.Name(), sfi.Mode().String())
+    }
+    _, err = os.Stat(dst)
+    if err != nil {
+        if !os.IsNotExist(err) {
+            panic(err)
+            return
+        }
+    }
+    err = copyFileContents(src, dst)
+    return
+}
+
+// copyFileContents copies the contents of the file named src to the file named
+// by dst. The file will be created if it does not already exist. If the
+// destination file exists, all it's contents will be replaced by the contents
+// of the source file.
+func copyFileContents(src, dst string) (err error) {
+    in, err := os.Open(src)
+    if err != nil {
+        return
+    }
+    defer in.Close()
+    out, err := os.Create(dst)
+    if err != nil {
+        return
+    }
+    defer func() {
+        cerr := out.Close()
+        if err == nil {
+            err = cerr
+        }
+    }()
+    if _, err = io.Copy(out, in); err != nil {
+        return
+    }
+    err = out.Sync()
+    return
 }
